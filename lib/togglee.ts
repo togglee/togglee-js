b1d95ea9b370c9c081318bd554975884a22c7d94
Alvaro <me@kanekotic.com>
2020-09-16 18:27:03 +0200

    initial functional commit

--- lib/togglee.ts
+++ lib/togglee.ts
@@ -1,1 +1,54 @@
-export class Togglee {}
+import axios from 'axios'
+
+const operations_maps: any = {
+    'eq': (first: any, second: any) => first == second,
+    'ne': (first: any, second: any) => first != second,
+    'gt': (first: any, second: any) => first > second,
+    'ge': (first: any, second: any) => first >= second,
+    'lt': (first: any, second: any) => first < second,
+    'le': (first: any, second: any) => first <= second,
+}
+const strategyMaps: any = {
+    "release": (toggle: any) => toggle['value'],
+    "context": (toggle: any, context: any) => {
+        for (const condition of toggle.conditions) {
+            const value = context[condition['field']]
+            const expected_value = condition['value']
+            const operation = condition['operation']
+            if(!operations_maps[operation](expected_value, value))
+                return false
+        }
+        return true
+    },
+}
+
+export class Togglee {
+  private toggles?: any
+  private url: string
+
+  constructor(url: string, refreshRate: number, defaults?: any) {
+    this.toggles = defaults
+    setInterval(this.refreshCache, refreshRate * 1000)
+    this.url = url
+  }
+
+  public isEnabled(prop: string, context?: any) {
+    return this.toggles !== undefined && this.toggles[prop] !== undefined ? this.getValue(prop, context) : false
+  }
+
+  private getValue(prop: string, context: any) {
+    try {
+      const toggle = this.toggles[prop]
+      return strategyMaps[toggle.type](toggle, context)
+    } catch (error) {
+      return false
+    }
+  }
+
+  private refreshCache = async () => {
+      try {
+        this.toggles = (await axios.get(this.url)).data
+      } catch (error) {
+      }
+  }
+}
--- test/togglee.spec.ts
+++ test/togglee.spec.ts
@@ -1,5 +1,85 @@
 import { Togglee } from '../lib/togglee'
+// @ts-ignore
+import simulado from 'simulado'
+import axios from 'axios'
 
 describe('Toggle', () => {
-  it('should do stuff', () => {})
+  beforeEach(async () => {
+    await simulado.start()
+  })
+
+  afterEach(async () => {
+    await simulado.stop()
+  })
+
+  // it('should do stuff', async () => {
+  //   const expectedResult = {
+  //     prop1: 'awesome',
+  //   }
+  //   await simulado.setMock({
+  //     path: '/somepath',
+  //     body: expectedResult,
+  //   })
+
+  //   const response = await axios.get('http://localhost:7001/somepath')
+  //   console.log(response)
+  // })
+
+  it('should use default value', async () => {
+    const expectedResult = {
+      "propTrue": {
+          "type": "release",
+          "value": true
+      },
+      "propFalse": {
+          "type": "release",
+          "value": false
+      }
+    }
+
+    const subject = new Togglee('http://localhost:7001/somepath', 1000, expectedResult)
+
+    expect(subject.isEnabled("propTrue")).toBeTruthy()
+    expect(subject.isEnabled("propFalse")).toBeFalsy()
+  })
+
+  it('should return false as default', async () => {
+
+    const subject = new Togglee('http://localhost:7001/somepath', 1000, {})
+
+    expect(subject.isEnabled("somerandom")).toBeFalsy()
+  })
+
+  it('should return false if no defaults', async () => {
+
+    const subject = new Togglee('http://localhost:7001/somepath', 1000)
+
+    expect(subject.isEnabled("somerandom")).toBeFalsy()
+  })
+
+  it('should refresh cache in rate', async () => {
+    const sleeper = new Promise(resolve => setTimeout(resolve, 5000))
+    const expectedResult = {
+      "propTrue": {
+          "type": "release",
+          "value": true
+      },
+      "propFalse": {
+          "type": "release",
+          "value": false
+      }
+    }
+
+    await simulado.setMock({
+      path: '/somepath',
+      body: expectedResult,
+    })
+
+    const subject = new Togglee('http://localhost:7001/somepath', 1)
+
+    await sleeper 
+    expect(subject.isEnabled("propTrue")).toBeTruthy()
+    expect(subject.isEnabled("propFalse")).toBeFalsy()
+  })
+
 })
